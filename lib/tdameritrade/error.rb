6da3b4680881ad760930a3313eaf8477a0fa7da4
Winston Kotzan <wak@wakproductions.com>
2019-08-24 15:36:27 -0400

    module_function

--- lib/tdameritrade/error.rb
+++ lib/tdameritrade/error.rb
@@ -1,12 +1,14 @@
 module TDAmeritrade
   module Error
+    module_function
+
     class TDAmeritradeError < StandardError
     end
 
     class RateLimitError < StandardError
     end
 
-    def self.gem_error(message)
+    def gem_error(message)
       error = TDAmeritradeError.new(message)
       error.set_backtrace(caller)
       raise error
