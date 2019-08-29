cba825fedf0f046a0a022c02b40f5af626fa018d
Winston Kotzan <wak@wakproductions.com>
2019-08-28 21:37:16 -0400

    Add note on how to use webmock off

--- spec/support/authenticated_client.rb
+++ spec/support/authenticated_client.rb
@@ -1,5 +1,7 @@
 shared_context 'authenticated client' do
   let!(:client) do
+    # If you turn off WebMock by including the 'webmock_off' shared context in your tests, you can hit the
+    # live API with a real ping. Just be sure to replace these values here with actual connection tokens.
     TDAmeritrade::Client.new(
       client_id: 'RUBYAPITEST@AMER.OAUTHAP',
       redirect_uri: 'http://localhost:3000',
